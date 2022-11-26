84a881896fc4fd14aad2dbe27a9f372035067047
phamquocanhust@gmail.com <phamquocanhust@gmail.com>
2022-11-27 01:53:59 +0900

    add common code for testing

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
@@ -22,9 +23,11 @@ require (
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
---
+++ test/util.go
@@ -0,0 +1,168 @@
+package test
+
+import (
+	"bytes"
+	"fmt"
+	"go_web/api/middleware"
+	"go_web/config"
+	"go_web/domain/entity"
+	"go_web/domain/service/implement"
+	repo "go_web/infra/repository/mysql"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"go_web/infra/repository/mysql"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+
+	"gorm.io/gorm"
+)
+
+func getApp() App {
+	cfg := config.LoadConfig()
+	if cfg == nil {
+		panic("fail to load config")
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.Mysql.User, cfg.Mysql.Password, cfg.Mysql.Host, cfg.Mysql.Port, cfg.Mysql.Db)
+	fmt.Printf("dns: %s", dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return App{
+		Config: cfg,
+		Db:     db,
+	}
+}
+
+var actorMap = map[string]entity.User{
+	"admin": {
+		ID:   1,
+		Name: "admin",
+		Type: entity.UserTypeSuper,
+	},
+}
+
+func createLoginMiddleware(app App) func(*gin.Context) {
+	userRepo := mysql.NewUserRepo(app.Db)
+	authService := implement.NewAuthService(app.Config, userRepo)
+
+	return middleware.NewLoginMiddleware(authService).Value()
+}
+
+func doLogin(app App, userName string, request *http.Request) {
+	user, ok := actorMap[userName]
+	if !ok {
+		panic(fmt.Sprintf("not found user with name: %s", userName))
+	}
+	userRepo := repo.NewUserRepo(app.Db)
+	authService := implement.NewAuthService(app.Config, userRepo)
+	token, err := authService.CreateToken(user)
+
+	if err != nil {
+		panic(err)
+	}
+
+	request.AddCookie(&http.Cookie{
+		Name:  "Authenticate",
+		Value: token,
+	})
+}
+
+type App struct {
+	Db     *gorm.DB
+	Config *config.Config
+}
+
+type Method string
+
+const (
+	GET    Method = "GET"
+	Post   Method = "POST"
+	Put    Method = "PUT"
+	Delete Method = "DELETE"
+)
+
+type Scenario struct {
+	Name         string
+	Actor        string
+	Request      []byte
+	BeforeTest   func(app App)
+	ExpectedCode int
+	AssertFunc   func(app App, t *testing.T, resRecorder *httptest.ResponseRecorder)
+	AfterTest    func(app App)
+}
+type TestData struct {
+	Url       string
+	Method    Method
+	Handler   func(ctx *gin.Context)
+	Scenarios []Scenario
+}
+
+func RunTest(t *testing.T, test TestData) {
+
+	if len(test.Scenarios) == 0 {
+		return
+	}
+
+	var appWithTx = (func() App {
+		app := getApp()
+		return App{
+			Config: app.Config,
+			Db:     app.Db.Begin(),
+		}
+	})()
+
+	defer appWithTx.Db.Rollback()
+
+	// create route with middleware & handler
+	router := gin.Default()
+	router.Use(createLoginMiddleware(appWithTx))
+	group := router.Group(test.Url)
+	switch test.Method {
+	case GET:
+		group.GET(test.Url, test.Handler)
+		break
+	case Post:
+		group.POST(test.Url, test.Handler)
+		break
+	case Put:
+		group.PUT(test.Url, test.Handler)
+		break
+	case Delete:
+		group.DELETE(test.Url, test.Handler)
+		break
+	default:
+		panic(fmt.Sprintf("un-support http method: %s", test.Method))
+	}
+
+	for _, item := range test.Scenarios {
+		if item.BeforeTest != nil {
+			item.BeforeTest(appWithTx)
+		}
+		request, err := http.NewRequest(string(test.Method), test.Url, bytes.NewBuffer(item.Request))
+		if err != nil {
+			panic(err)
+		}
+		doLogin(appWithTx, item.Actor, request)
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, request)
+
+		assert.Equal(t, item.ExpectedCode, w.Code, item.Name)
+		
+		if item.AssertFunc != nil {
+			item.AssertFunc(appWithTx, t, w)
+		}
+
+		if item.AfterTest != nil {
+			item.AfterTest(appWithTx)
+		}
+	}
+
+}
